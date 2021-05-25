ffae5b1d634bf2d839238382ee6fa5a7bb8d2a96
SyamaM <syamamanju22@gmail.com>
2021-05-25 21:45:46 +0530

    solving using switchcase statement

---
+++ switchcase.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -x
+salary=20
+rancheck=$((RANDOM%3))
+case $rancheck in
+        0) echo "the employee is absent"
+           wrkhr=0
+           ;;
+        1) echo "the employee is present"
+           wrkhr=10
+           ;;
+        2) echo "employee is part time present"
+           wrkhr=8
+           ;;
+        *) echo "not in the above category"
+           ;;
+esac
+
+ income=$(($salary*$wrkhr))
+ echo "$income"
