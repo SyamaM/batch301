f6150c61696c685dbf10b0e5113903c280c68627
SyamaM <syamamanju22@gmail.com>
2021-05-26 21:29:43 +0530

    calculate daily employee wage

--- empdailywage.sh
+++ empdailywage.sh
@@ -3,12 +3,12 @@ rancheck=$((RANDOM%2))
 salary=20
 if [ $rancheck -eq 0 ]
 then
-	echo "the employee is absent"
-	wrkhrs=0
-	dailywage=$(($wrkhrs*$salary))
+        echo "the employee is absent"
+        wrkhrs=0
+        dailywage=$(($wrkhrs*$salary))
 else
-	echo "employee is present"
-	wrkhrs=8
-	dailywage=$(($wrkhrs*$salary))
+        echo "employee is present"
+        wrkhrs=8
+        dailywage=$(($wrkhrs*$salary))
 fi
 echo "$dailywage"
